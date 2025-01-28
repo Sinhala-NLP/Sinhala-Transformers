0e5e6a741abfaf0213e462c8c04d8c232a9357f7
TharinduDR <rhtdranasinghe@gmail.com>
2025-01-28 12:58:06 +0000

    001: Initial commit

--- experiments/train_lm.py
+++ experiments/train_lm.py
@@ -18,6 +18,7 @@ arguments = parser.parse_args()
 dataset = Dataset.to_pandas(load_dataset('sinhala-nlp/sinhala-7m-corpus', split='train'))
 lines = dataset['text'].tolist()
 
+lines = lines[:500000]
 shuffle(lines)
 train_lines = lines[:int(len(lines)*.8)]
 test_lines = lines[int(len(lines)*.8):len(lines)]
