57fdf81e014ad8769e3df3e0c1ed18e4a45178d2
TharinduDR <rhtdranasinghe@gmail.com>
2025-01-30 19:47:16 +0000

    001: Initial commit

--- experiments/train_lm.py
+++ experiments/train_lm.py
@@ -22,7 +22,6 @@ VOCAB_SIZE = int(arguments.vocab_size)
 dataset = Dataset.to_pandas(load_dataset('sinhala-nlp/sinhala-7m-corpus', split='train'))
 lines = dataset['text'].tolist()
 
-lines = lines[:5000]
 shuffle(lines)
 train_lines = lines[:int(len(lines)*.8)]
 test_lines = lines[int(len(lines)*.8):len(lines)]
