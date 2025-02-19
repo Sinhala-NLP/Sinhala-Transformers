8482bcde1a019fe7aeeb21737e85f51e7f358ec9
TharinduDR <rhtdranasinghe@gmail.com>
2025-02-19 15:48:48 +0000

    001: Initial commit

---
+++ experiments/roberta_tockeniser.py
@@ -0,0 +1,24 @@
+from datasets import load_dataset
+from tokenizers import trainers, Tokenizer, normalizers, ByteLevelBPETokenizer
+
+# load dataset
+dataset = load_dataset('sinhala-nlp/sinhala-7m-corpus', split='train')
+
+# Instantiate tokenizer
+tokenizer = ByteLevelBPETokenizer()
+
+def batch_iterator(batch_size=1000):
+    for i in range(0, len(dataset), batch_size):
+        yield dataset[i: i + batch_size]["text"]
+
+# Customized training
+tokenizer.train_from_iterator(batch_iterator(), vocab_size=50265, min_frequency=2, special_tokens=[
+    "<s>",
+    "<pad>",
+    "</s>",
+    "<unk>",
+    "<mask>",
+])
+
+# Save files to disk
+tokenizer.save("./sinhala-roberta-base/tokenizer.json")
