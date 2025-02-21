2fca3bc4d2cc50f96366a64804bb5eece6956877
TharinduDR <rhtdranasinghe@gmail.com>
2025-02-21 15:37:17 +0000

    001: Initial commit

--- experiments/roberta_tockeniser.py
+++ experiments/roberta_tockeniser.py
@@ -1,24 +1,32 @@
 from datasets import load_dataset
-from tokenizers import trainers, Tokenizer, normalizers, ByteLevelBPETokenizer
+from tokenizers import Tokenizer, normalizers, trainers, models, pre_tokenizers
 
-# load dataset
+# Load dataset
 dataset = load_dataset('sinhala-nlp/sinhala-7m-corpus', split='train')
 
-# Instantiate tokenizer
-tokenizer = ByteLevelBPETokenizer()
+# Instantiate a BPE tokenizer
+tokenizer = Tokenizer(models.BPE())
 
+# Set normalizer (optional)
+tokenizer.normalizer = normalizers.Sequence([normalizers.NFKC()])
+
+# Pre-tokenizer (splits text into words before BPE)
+tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel()
+
+# Training function using dataset iterator
 def batch_iterator(batch_size=1000):
     for i in range(0, len(dataset), batch_size):
         yield dataset[i: i + batch_size]["text"]
 
-# Customized training
-tokenizer.train_from_iterator(batch_iterator(), vocab_size=32000, min_frequency=2, special_tokens=[
-    "<s>",
-    "<pad>",
-    "</s>",
-    "<unk>",
-    "<mask>",
-])
+# Define trainer
+trainer = trainers.BpeTrainer(
+    vocab_size=32000,
+    min_frequency=2,
+    special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>"]
+)
 
-# Save files to disk
-tokenizer.save("./sinhala-roberta-base/tokenizer.json")
+# Train tokenizer
+tokenizer.train_from_iterator(batch_iterator(), trainer)
+
+# Save tokenizer
+tokenizer.save("sinhala-roberta-base/tokenizer.json")
