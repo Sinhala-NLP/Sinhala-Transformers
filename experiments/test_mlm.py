3167a4ee247008512a91e968a86b34866f1dbe90
TharinduDR <rhtdranasinghe@gmail.com>
2025-03-04 13:42:27 +0000

    001: Initial commit

--- experiments/test_mlm.py
+++ experiments/test_mlm.py
@@ -12,7 +12,7 @@ model = AutoModelForMaskedLM.from_pretrained(model_path, from_flax=True)
 fill_mask = pipeline("fill-mask", model=model, tokenizer=tokenizer)
 
 # Test sentence
-sentence = "නිවේදනයක් නිකුත් කරමින් <mask> මාධ්‍ය කෙට්ඨාසය මේ බව දැනුම් දී තිබේ."
+sentence = "<mask> තැබීම සිදුකළ දිනයේ සිට මේ වන තෙක් ඇය සැඟව සිටින ස්ථානය සම්බන්ධයෙන් නිසි තොරතුරක් නොමැති බැවින් සැකකාරිය අත්අඩංගුවට ගැනීම සඳහා සාර්ථක තොරතුරක් ලබාදෙන අයෙකුට රුපියල් ලක්ෂ 12 ක මුදල් ත්‍යාගයක් පිරිනැමීමට පොලිස් මූලස්ථානය තීරණය කර ඇත"
 
 # Get predictions
 predictions = fill_mask(sentence)
